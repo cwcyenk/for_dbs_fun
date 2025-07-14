c6ad421b198383f73d1e2ace0acdba270f685bc7
Yen Kai <cwcyenk@yahoo.com.sg>
2025-07-14 15:49:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+
+import os
+#https://console.groq.com/keys
+os.environ['GROQ_API_KEY'] = "groq_key"
+# for cloud .......
 
 app = Flask(__name__)
 
@@ -13,6 +19,11 @@ def main():
 
     # db
     return(render_template("main.html"))
+
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    q = request.form.get("q")
+    return(render_template("llama.html"))
 
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
@@ -30,6 +41,22 @@ def prediction():
     pred = model.predict([[q]])
 
     return(render_template("prediction.html",r=pred))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html", r=completion.choices[0].message.content))
 
 #@app.route("/prediction",methods=["GET","POST"])
 #def prediction():

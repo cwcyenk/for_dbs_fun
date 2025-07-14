8aa2019d1dbecaf7fef277453b7005f11c245692
Yen Kai <cwcyenk@yahoo.com.sg>
2025-07-14 12:33:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def prediction():
     # make prediction
     pred = model.predict([[q]])
 
-    return(render_template("prediction.html",r=pred)
+    return(render_template("prediction.html",r=pred))
+
+#@app.route("/prediction",methods=["GET","POST"])
+#def prediction():
+#    q = float(request.form.get("q"))
+#    return(render_template("prediction.html",r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run()

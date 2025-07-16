99633c10db37ada3437ab46634fe12bc52c7134a
Yen Kai <cwcyenk@yahoo.com.sg>
2025-07-16 13:43:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,6 @@ def stop_telegram():
     delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
     webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
 
-    # Set the webhook URL for the Telegram bot
-    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
-    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
-
     if webhook_response.status_code == 200:
         # set status message
         status = "The telegram bot is running. Please check with the telegram bot. @dsai_hamster_ft1_bot"

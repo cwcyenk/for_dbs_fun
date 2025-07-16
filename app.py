4431273b6d701322c51a3f1b65f4a538c7105a3c
Yen Kai <cwcyenk@yahoo.com.sg>
2025-07-16 13:48:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ def stop_telegram():
 
     if webhook_response.status_code == 200:
         # set status message
-        status = "The telegram bot is running. Please check with the telegram bot. @dsai_hamster_ft1_bot"
+        status = "The telegram bot is stopped. Please check with the telegram bot. @dsai_hamster_ft1_bot"
     else:
-        status = "Failed to start the telegram bot. Please check the logs."
+        status = "Failed to stop the telegram bot. Please check the logs."
     return(render_template("telegram.html", status=status))
 
 @app.route("/dbs",methods=["GET","POST"])
